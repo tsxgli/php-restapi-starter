9110dee57ca4d0eb01d04ddb68906bdb65071c8d
Ahrnuld <mark.dehaan@inholland.nl>
2022-12-21 16:18:22 +0100

    Added if statements for GET and POST requests

--- app/api/controllers/articlecontroller.php
+++ app/api/controllers/articlecontroller.php
@@ -1,21 +1,37 @@
 <?php
 require __DIR__ . '/../../services/articleservice.php';
 
-class ArticleController {
+class ArticleController
+{
 
-    private $articleService; 
+    private $articleService;
 
     // initialize services
-    function __construct() {
+    function __construct()
+    {
         $this->articleService = new ArticleService();
     }
 
-    public function index() {
-      
-        // your code here
-        // return all articles in the database as JSON
-        
-        return null;       
+    // router maps this to /api/article automatically
+    public function index()
+    {
+
+        // Respond to a GET request to /api/article
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+            // your code here
+            // return all articles in the database as JSON
+
+        }
+
+        // Respond to a POST request to /api/article
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            // your code here
+            // read JSON from the request, convert it to an article object
+            // and have the service insert the article into the database
+
+        }
     }
 }
 ?>
